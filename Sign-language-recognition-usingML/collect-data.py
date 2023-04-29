647c68d1d574089128ac3392399dd3f77e1b2dda
IGNECIA RATHNA M <73903767+Igneciarathna@users.noreply.github.com>
2023-04-29 17:28:04 +0530

    Add files via upload

---
+++ Sign-language-recognition-usingML/collect-data.py
@@ -0,0 +1,208 @@
+import cv2
+import numpy as np
+import os
+
+# Create the directory structure
+if not os.path.exists("C:/Users/M IGNECIA RATHNA/Desktop/number-sign-recognition-master/data"):
+    os.makedirs("C:/Users/M IGNECIA RATHNA/Desktop/number-sign-recognition-master/data")
+    os.makedirs("data/train")
+    os.makedirs("data/test")
+    os.makedirs("data/train/A")
+    os.makedirs("data/train/B")
+    os.makedirs("data/train/C")
+    os.makedirs("data/train/D")
+    os.makedirs("data/train/E")
+    os.makedirs("data/train/F")
+    os.makedirs("data/train/G")
+    os.makedirs("data/train/H")
+    os.makedirs("data/train/I")
+    os.makedirs("data/train/J")
+    os.makedirs("data/train/K")
+    os.makedirs("data/train/L")
+    os.makedirs("data/train/M")
+    os.makedirs("data/train/N")
+    os.makedirs("data/train/O")
+    os.makedirs("data/train/P")
+    os.makedirs("data/train/Q")
+    os.makedirs("data/train/R")
+    os.makedirs("data/train/S")
+    os.makedirs("data/train/T")
+    os.makedirs("data/train/U")
+    os.makedirs("data/train/V")
+    os.makedirs("data/train/W")
+    os.makedirs("data/train/X")
+    os.makedirs("data/train/Y")
+    os.makedirs("data/train/Z")
+    os.makedirs("data/train/iloveyou")
+    os.makedirs("data/test/A")
+    os.makedirs("data/test/B")
+    os.makedirs("data/test/C")
+    os.makedirs("data/test/D")
+    os.makedirs("data/test/E")
+    os.makedirs("data/test/F")
+    os.makedirs("data/test/G")
+    os.makedirs("data/test/H")
+    os.makedirs("data/test/I")
+    os.makedirs("data/test/J")
+    os.makedirs("data/test/K")
+    os.makedirs("data/test/L")
+    os.makedirs("data/test/M")
+    os.makedirs("data/test/N")
+    os.makedirs("data/test/O")
+    os.makedirs("data/test/P")
+    os.makedirs("data/test/Q")
+    os.makedirs("data/test/R")
+    os.makedirs("data/test/S")
+    os.makedirs("data/test/T")
+    os.makedirs("data/test/U")
+    os.makedirs("data/test/V")
+    os.makedirs("data/test/W")
+    os.makedirs("data/test/X")
+    os.makedirs("data/test/Y")
+    os.makedirs("data/test/Z")
+    os.makedirs("data/test/iloveyou")
+
+# Train or test 
+mode = 'train'
+directory = 'data/'+mode+'/'
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    # Simulating mirror image
+    frame = cv2.flip(frame, 1)
+    
+    # Getting count of existing images
+    count = {'a': len(os.listdir(directory+"/A")),
+             'b': len(os.listdir(directory+"/B")),
+             'c': len(os.listdir(directory+"/C")),
+             'd': len(os.listdir(directory+"/D")),
+             'e': len(os.listdir(directory+"/E")),
+             'f': len(os.listdir(directory+"/F")),
+             'g': len(os.listdir(directory+"/G")),
+             'h': len(os.listdir(directory+"/H")),
+             'i': len(os.listdir(directory+"/I")),
+             'j': len(os.listdir(directory+"/J")),
+             'k': len(os.listdir(directory+"/K")),
+             'l': len(os.listdir(directory+"/L")),
+             'm': len(os.listdir(directory+"/M")),
+             'n': len(os.listdir(directory+"/N")),
+             'o': len(os.listdir(directory+"/O")),
+             'p': len(os.listdir(directory+"/P")),
+             'q': len(os.listdir(directory+"/Q")),
+             'r': len(os.listdir(directory+"/R")),
+             's': len(os.listdir(directory+"/S")),
+             't': len(os.listdir(directory+"/T")),
+             'u': len(os.listdir(directory+"/U")),
+             'v': len(os.listdir(directory+"/V")),
+             'w': len(os.listdir(directory+"/W")),
+             'x': len(os.listdir(directory+"/X")),
+             'y': len(os.listdir(directory+"/Y")),
+             'z': len(os.listdir(directory+"/Z")),
+             'iloveyou': len(os.listdir(directory+"/iloveyou")),}
+    
+    # Printing the count in each set to the screen
+    cv2.putText(frame, "MODE : "+mode, (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "IMAGE COUNT", (10, 100), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "A : "+str(count['a']), (10, 120), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "B : "+str(count['b']), (10, 140), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "C : "+str(count['c']), (10, 160), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "D : "+str(count['d']), (10, 180), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "E : "+str(count['e']), (10, 200), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "F : "+str(count['f']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "iloveyou : "+str(count['iloveyou']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    
+    # Coordinates of the ROI
+    x1 = int(0.5*frame.shape[1])
+    y1 = 10
+    x2 = frame.shape[1]-10
+    y2 = int(0.5*frame.shape[1])
+    # Drawing the ROI
+    # The increment/decrement by 1 is to compensate for the bounding box
+    cv2.rectangle(frame, (x1-1, y1-1), (x2+1, y2+1), (255,0,0) ,1)
+    # Extracting the ROI
+    roi = frame[y1:y2, x1:x2]
+    roi = cv2.resize(roi, (64, 64)) 
+ 
+    cv2.imshow("Frame", frame)
+    
+    #_, mask = cv2.threshold(mask, 200, 255, cv2.THRESH_BINARY)
+    #kernel = np.ones((1, 1), np.uint8)
+    #img = cv2.dilate(mask, kernel, iterations=1)
+    #img = cv2.erode(mask, kernel, iterations=1)
+    # do the processing after capturing the image!
+    roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    _, roi = cv2.threshold(roi, 120, 255, cv2.THRESH_BINARY)
+    cv2.imshow("ROI", roi)
+    
+    interrupt = cv2.waitKey(10)
+    if interrupt & 0xFF == 27: # esc key
+        break
+    if interrupt & 0xFF == ord('a'):
+        cv2.imwrite(directory+'A/'+str(count['a'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('b'):
+        cv2.imwrite(directory+'B/'+str(count['b'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('c'):
+        cv2.imwrite(directory+'C/'+str(count['c'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('d'):
+        cv2.imwrite(directory+'D/'+str(count['d'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('e'):
+        cv2.imwrite(directory+'E/'+str(count['e'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('f'):
+        cv2.imwrite(directory+'F/'+str(count['f'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('g'):
+        cv2.imwrite(directory+'G/'+str(count['g'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('h'):
+        cv2.imwrite(directory+'H/'+str(count['h'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('i'):
+        cv2.imwrite(directory+'I/'+str(count['i'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('j'):
+        cv2.imwrite(directory+'J/'+str(count['j'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('k'):
+        cv2.imwrite(directory+'K/'+str(count['k'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('l'):
+        cv2.imwrite(directory+'L/'+str(count['l'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('m'):
+        cv2.imwrite(directory+'M/'+str(count['m'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('n'):
+        cv2.imwrite(directory+'N/'+str(count['n'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('o'):
+        cv2.imwrite(directory+'O/'+str(count['o'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('p'):
+        cv2.imwrite(directory+'P/'+str(count['p'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('q'):
+        cv2.imwrite(directory+'Q/'+str(count['q'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('r'):
+        cv2.imwrite(directory+'R/'+str(count['r'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('s'):
+        cv2.imwrite(directory+'S/'+str(count['s'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('t'):
+        cv2.imwrite(directory+'T/'+str(count['t'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('u'):
+        cv2.imwrite(directory+'U/'+str(count['u'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('v'):
+        cv2.imwrite(directory+'V/'+str(count['v'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('w'):
+        cv2.imwrite(directory+'W/'+str(count['w'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('x'):
+        cv2.imwrite(directory+'X/'+str(count['x'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('Y'):
+        cv2.imwrite(directory+'Y/'+str(count['y'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('z'):
+        cv2.imwrite(directory+'Z/'+str(count['z'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('1'):
+        cv2.imwrite(directory+'iloveyou/'+str(count['iloveyou'])+'.jpg', roi)
+    
+cap.release()
+cv2.destroyAllWindows()
+"""
+d = "old-data/test/0"
+newd = "data/test/0"
+for walk in os.walk(d):
+    for file in walk[2]:
+        roi = cv2.imread(d+"/"+file)
+        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        _, mask = cv2.threshold(roi, 120, 255, cv2.THRESH_BINARY)
+        cv2.imwrite(newd+"/"+file, mask)     
+"""
